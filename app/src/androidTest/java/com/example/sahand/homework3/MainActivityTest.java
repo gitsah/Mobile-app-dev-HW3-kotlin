03b32dddba2d00c0172837ba8d9a49f48483bb3f
Sahand-Admin <ghotbijr@gmail.com>
2018-04-26 17:49:16 -0700

    Test changes

--- app/src/androidTest/java/com/example/sahand/homework3/MainActivityTest.java
+++ app/src/androidTest/java/com/example/sahand/homework3/MainActivityTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -23,9 +24,10 @@ public class MainActivityTest {
 
     @Test
     public void notEighteen() {
-        onView(withId(R.id.name_field)).perform(typeText("Steve L"));
-        onView(withId(R.id.date_of_birth_field)).perform(typeText("02/10/1999"));
-        onView(withId(R.id.email_field)).perform(typeText("jackson@gmail.com"));
+        onView(withId(R.id.name_field)).perform(typeText("Steve L")).perform(closeSoftKeyboard());
+        onView(withId(R.id.date_of_birth_field)).perform(typeText("02/10/2003")).perform(closeSoftKeyboard());
+        onView(withId(R.id.username_field)).perform(typeText("SteveL")).perform(closeSoftKeyboard());
+        onView(withId(R.id.email_field)).perform(typeText("jackson@gmail.com")).perform(closeSoftKeyboard());
         onView(withId(R.id.submit_button)).perform(click());
         onView(withId(R.id.validation_text)).check(matches(withText(R.string.underEighteen)));
     }
